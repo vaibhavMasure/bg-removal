e9ec1fea471a1ddaab86b0d34ceba9c70b9f5923
Vaibhav Masure <vaibhavmasure123@gmail.com>
2025-01-04 13:33:43 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+
+// app config
+
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+// Initialize middlewares
+app.use(express.json())
+app.use(cors())
+
+// API routes
+
+app.get('/', (req, res)=>res.send('API WORKING'))
+
+app.listen(PORT, ()=> console.log('Server running on port' + PORT));
